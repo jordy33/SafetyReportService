dac7e8e635a86fafce77e8bf4b586707f4a4eb23
Jorge <jorgemacias@hotmail.com>
2014-01-10 14:32:49 -0600

    commit

---
+++ src/MyCompany/Controller/RpcController.php
@@ -0,0 +1,22 @@
+<?php
+namespace MyCompany\Controller;
+
+use ZendServerGateway\Controller\AbstractActionController;
+use MyCompany\Model\CustomerRepository;
+
+class RpcController extends AbstractActionController
+{
+
+    public function getHelloAction ($name, $surname)
+    {
+        return array(
+            'message' => "Hello $name $surname!"
+        );
+    }
+
+    public function getCustomersAction ()
+    {
+        $cr = new CustomerRepository();
+        return $cr->getAll();
+    }
+}
